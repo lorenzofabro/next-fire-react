eba82a7eb35f67eb53b10e18a8241be284629abd
Lorenzo Fabro <lorenzofabro@Lorenzos-MacBook-Pro.local>
2021-12-20 01:45:31 -0300

    404 custom page, new post content & metatags 🍉

--- pages/admin/[slug].tsx
+++ pages/admin/[slug].tsx
@@ -1,6 +1,9 @@
+import Metatags from "../../components/Metatags";
+
 export default function AdminPostEdit({}) {
   return (
     <main>
+      <Metatags title="Admin page" />
       <h1>Admin posts edit!</h1>
     </main>
   );
